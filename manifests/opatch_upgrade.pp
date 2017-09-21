1b80ac0764c601678e618f25d77533564a2e20f5
Richard Abbenhuis <richard@abbenhuis.net>
2017-09-21 13:49:41 +0200

    Removed parameter package_cleanup

--- manifests/opatch_upgrade.pp
+++ manifests/opatch_upgrade.pp
@@ -8,7 +8,6 @@
 # @param package_source the location where the installation software is available
 # @param package_name filename of the opatch upgrade file
 # @param package_target location for installation files used by this module
-# @param package_cleanup should the installation files and directories be removed afterwards
 # @param opatch_version opatch version of the opatch upgrade file
 # @param csi_number Oracle support csi number
 # @param support_id Oracle support id
@@ -17,11 +16,10 @@
 define oracledb::opatch_upgrade (
   String            $oracle_home     = undef,
   String            $os_user         = 'oracle',
-  String            $os_group        = 'dba',
+  String            $os_group        = 'oinstall',
   String            $package_source  = 'puppet:///modules/oracledb/',
   String            $package_name    = undef,
   String            $package_target  = '/var/tmp/install',
-  Boolean           $package_cleanup = true,
   String            $opatch_version  = undef,
   Optional[Integer] $csi_number      = undef,
   Optional[String]  $support_id      = undef,
@@ -35,134 +33,71 @@ define oracledb::opatch_upgrade (
   $execution_path = lookup('oracledb::execution_path')
   $opatch_dir = "${oracle_home}/OPatch"
 
-  # Check if oracle_home exists
-  $oracle_home_found = oracledb::oracle_home_exists($oracle_home)
+  if ($package_source =~ /^puppet/) {
+    # Puppet install
+    file { "${package_target}/${package_name}":
+      ensure => present,
+      owner  => $os_user,
+      group  => $os_group,
+      mode   => '0664',
+      source => "${package_source}/${package_name}",
+    }
 
-  if $oracle_home_found == undef {
-    $continue_upgrade = false
+    $install_source = $package_target
   }
   else {
-    if $oracle_home_found {
-      # Check the opatch version
-      $installed_opatch_version = oracledb::current_opatch_version($oracle_home)
-
-      if ($installed_opatch_version == undef) or ($installed_opatch_version == 'NotFound') {
-        $continue_upgrade = false
-      }
-      else {
-        if ($installed_opatch_version == $opatch_version) {
-          $continue_upgrade = false
-        }
-        else {
-          notify { "Current OPatch version (${installed_opatch_version}) doesn't equal ${opatch_version}. So patching is required": }
-          $continue_upgrade = true
-        }
-      }
-    }
-    else {
-      $continue_upgrade = false
-    }
+    # Local install
+    $install_source = $package_source
   }
 
-  if ($continue_upgrade) {
-    if ($package_source =~ /^puppet/) {
-      # Puppet install
-      file { "${package_target}/${package_name}":
-        ensure => present,
-        owner  => $os_user,
-        group  => $os_group,
-        mode   => '0664',
-        source => "${package_source}/${package_name}",
-      }
+  dbs_opatch_upgrade { $package_name:
+    ensure        => present,
+    oracle_home   => $oracle_home,
+    install_dir   => $install_source,
+    patch_version => $opatch_version,
+  }
 
-      $install_source = $package_target
+  if ($opatch_version < '12.2.0.1.5') {
+    if (($csi_number != undef) and ($support_id != undef)) {
+      exec { "Execute emocmrsp ${title} ${opatch_version}":
+        command     => "${opatch_dir}/ocm/bin/emocmrsp -repeater NONE ${csi_number} ${support_id}",
+        cwd         => $opatch_dir,
+        environment => ["ORACLE_HOME=${oracle_home}"],
+        path        => $execution_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => Dbs_opatch_upgrade[$package_name],
+      }
     }
     else {
-      # Local install
-      $install_source = $package_source
-    }
-
-    # Create a backup of the current OPatch directory
-    file { "${opatch_dir}_${installed_opatch_version}":
-      ensure  => directory,
-      source  => "file://${opatch_dir}",
-      recurse => true,
-      before  =>  File[$opatch_dir],
-    }
-
-    # Delete opatch directory
-    file { $opatch_dir:
-      ensure  => absent,
-      recurse => true,
-      force   => true,
-      alias   => "Delete ${opatch_dir}",
-    }
-
-    # Extract the opatch upgrade file
-    exec { "Extract ${install_source}/${package_name}":
-      command   => "unzip -o ${install_source}/${package_name} -d ${oracle_home}",
-      path      => $execution_path,
-      user      => $os_user,
-      group     => $os_group,
-      logoutput => $log_output,
-      require   => File["Delete ${opatch_dir}"],
-    }
-
-    if ($opatch_version < '12.2.0.1.5') {
-      if (($csi_number != undef) and ($support_id != undef)) {
-        exec { "Execute emocmrsp ${title} ${opatch_version}":
-          command     => "${opatch_dir}/ocm/bin/emocmrsp -repeater NONE ${csi_number} ${support_id}",
-          cwd         => $opatch_dir,
-          environment => ["ORACLE_HOME=${oracle_home}"],
-          path        => $execution_path,
-          user        => $os_user,
-          group       => $os_group,
-          logoutput   => $log_output,
-          require     => File["Extract ${install_source}/${package_name}"],
+      # Install expect
+      if !defined(Package['expect']) {
+        package { 'expect':
+          ensure => present,
         }
       }
-      else {
-        # Install expect
-        if !defined(Package['expect']) {
-          package { 'expect':
-            ensure => present,
-          }
-        }
 
-        file { "${package_target}/opatch_upgrade_${title}_${opatch_version}.sh":
-          ensure  => present,
-          owner   => $os_user,
-          group   => $os_group,
-          mode    => '0775',
-          content => epp('oracledb/ocm.rsp.epp', { 'opatch_dir' => $opatch_dir }),
-          require => Package['expect'],
-        }
-
-        exec { "Execute ${package_target}/opatch_upgrade_${title}_${opatch_version}.sh":
-          command     => "/bin/bash ${package_target}/opatch_upgrade_${title}_${opatch_version}.sh",
-          cwd         => $opatch_dir,
-          environment => ["ORACLE_HOME=${oracle_home}"],
-          path        => $execution_path,
-          user        => $os_user,
-          group       => $os_group,
-          logoutput   => $log_output,
-          require     => [
-            File["${package_target}/opatch_upgrade_${title}_${opatch_version}.sh"],
-            Exec["Extract ${install_source}/${package_name}"]],
-        }
+      file { "${package_target}/opatch_upgrade_${title}_${opatch_version}.sh":
+        ensure  => present,
+        owner   => $os_user,
+        group   => $os_group,
+        mode    => '0775',
+        content => epp('oracledb/ocm.rsp.epp', { 'opatch_dir' => $opatch_dir }),
+        require => Package['expect'],
       }
-    }
 
-    if ($package_cleanup) {
-      if ($package_source =~ /^puppet/) {
-        exec { "Remove ${package_target}/${package_name}":
-          command   => "rm -f ${package_target}/${package_name}",
-          user      => 'root',
-          group     => 'root',
-          logoutput => $log_output,
-          path      => $execution_path,
-          require   => Exec["Extract ${install_source}/${package_name}"],
-        }
+      exec { "Execute ${package_target}/opatch_upgrade_${title}_${opatch_version}.sh":
+        command     => "/bin/bash ${package_target}/opatch_upgrade_${title}_${opatch_version}.sh",
+        cwd         => $opatch_dir,
+        environment => ["ORACLE_HOME=${oracle_home}"],
+        path        => $execution_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => [
+          File["${package_target}/opatch_upgrade_${title}_${opatch_version}.sh"],
+          Dbs_opatch_upgrade[$package_name]],
       }
     }
   }
