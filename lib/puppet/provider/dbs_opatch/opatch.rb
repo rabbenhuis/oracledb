561ae914e22ec04e10bc9c7db3d64ca88ae72c02
Richard Abbenhuis <richard@abbenhuis.net>
2017-10-10 16:42:31 +0200

    Add initial version

---
+++ lib/puppet/provider/dbs_opatch/opatch.rb
@@ -0,0 +1,78 @@
+# opatch.rb
+#
+Puppet::Type.type(:dbs_opatch).provide(:opatch) do
+  desc 'Patch the Oracle Database Software'
+
+  def get_list_of_patches
+    oracle_home = resource[:oracle_home]
+    user        = resource[:os_user]
+    group       = resource[:os_group]
+    patches     = ''
+
+    command = oracle_home + '/OPatch/opatch lsinventory -patch_id -oh ' + oracle_home + ' -invPtrLoc /etc/oraInst.loc'
+    Puppet.debug "get_list_of_patches - command: #{command}"
+
+    output = execute(command, { :failonfail => true, :combine => true, :uid => user, :gid => group } )
+    Puppet.debug "get_list_of_patches - output: #{output}"
+
+    if output.scan(/no Interim patches installed/).empty?
+      output.each_line do |line|
+        if line.match(/^Patch/) and line.match(/: applied on/)
+          patch = line[5, line.index(':') - 5].strip
+          patches += patch + ';'
+        end
+      end
+    end
+
+    Puppet.debug "get_list_of_patches - patches: #{patches}"
+    return patches.split(";")
+  end
+
+  def get_patch_properties(patch_id)
+    get_list_of_patches.collect do |patch|
+      if patch == patch_id
+        return patch
+      end
+    end
+
+    return nil
+  end
+
+  def opatch(action)
+    oracle_home = resource[:oracle_home]
+    user        = resource[:os_user]
+    group       = resource[:os_group]
+    ocmrf_file  = resource[:ocmrf_file]
+    patch_dir   = resource[:patch_dir]
+
+    if ocmrf_file.nil?
+      ocmrf = ''
+    else
+      ocmrf = ' -ocmrf ' + ocmrf_file
+    end
+
+    if action == 'apply'
+      command = "#{oracle_home}/OPatch/opatch apply -silent #{ocmrf} -oh #{oracle_home} #{patch_dir}"
+    else
+      command = "#{oracle_home}/OPatch/opatch rollback -id #{patch_id} -silent -oh #{oracle_home}"
+    end
+    Puppet.debug "opatch - command: #{command}"
+
+    output = execute(command, { :failonfail => true, :combine => true, :uid => user, :gid => group } )
+    Puppet.debug "opatch - output: #{output}"
+  end
+
+  def exists?
+    get_patch_properties(resource[:name]) != nil
+  end
+
+  def destroy
+    Puppet.info("Rollback patch #{resource[:name]}")
+    opatch('rollback')
+  end
+
+  def create
+    Puppet.info("Apply patch #{resource[:name]}")
+    opatch('apply')
+  end
+end
---
+++ manifests/opatch.pp
@@ -0,0 +1,77 @@
+# opatch
+#
+# Install Oracle Database Server Patches
+#
+# @param oracle_home full path to the Oracle Home directory
+# @param patch_id the id of the opatch
+# @param patch_file the opatch patch file
+# @param patch_source the location where the opatch patch file is available
+# @param os_user the operating system user for using the Oracle software
+# @param os_group the operating group name for using the Oracle software
+# @param install_dir location for response file used by netca
+# @param log_output show all the output of the the exec actions
+# @param ocmrf should the ocm response file be used
+#
+define oracledb::opatch (
+  Enum['present', 'absent'] $ensure       = 'present',
+  String                    $oracle_home  = undef,
+  String                    $patch_id     = undef,
+  String                    $patch_file   = undef,
+  String                    $patch_source = 'puppet:///modules/oracledb',
+  String                    $os_user      = 'oracle',
+  String                    $os_group     = 'dba',
+  String                    $install_dir  = '/var/tmp/install',
+  Boolean                   $log_output   = false,
+  Boolean                   $ocmrf        = false,
+) {
+  $supported_kernels = join(lookup('oracledb::kernels'), '|')
+  if ($facts['kernel'] in $supported_kernels == false) {
+    fail("Unrecognized operating system, please use it on a ${supported_kernels} host")
+  }
+
+  $execution_path = lookup('oracledb::execution_path')
+
+  if ($patch_source =~ /^puppet/) {
+    if !defined(File["${install_dir}/${patch_file}"]) {
+      file { "${install_dir}/${patch_file}":
+        ensure => present,
+        source => "${patch_source}/${patch_file}",
+        owner  => $os_user,
+        group  => $os_group,
+        mode   => '0775',
+        before => Exec["Extract opatch ${patch_file} ${title}"],
+      }
+    }
+
+    $install_source = $install_dir
+  }
+  else {
+    $install_source = $patch_source
+  }
+
+  exec { "Extract opatch ${patch_file} ${title}":
+    command   => "unzip -n ${install_source}/${patch_file} -d ${install_dir}",
+    path      => $execution_path,
+    user      => $os_user,
+    group     => $os_group,
+    logoutput => $log_output,
+    creates   => "${install_dir}/${patch_id}",
+    before    => Dbs_opatch[$patch_id],
+  }
+
+  if ($ocmrf) {
+    dbs_opatch { $patch_id:
+      ensure      => $ensure,
+      oracle_home => $oracle_home,
+      patch_dir   => "${install_dir}/${patch_id}",
+      ocmrf_file  => "${oracle_home}/OPatch/ocm.rsp",
+    }
+  }
+  else {
+    dbs_opatch { $patch_id:
+      ensure      => $ensure,
+      oracle_home => $oracle_home,
+      patch_dir   => "${install_dir}/${patch_id}",
+    }
+  }
+}
